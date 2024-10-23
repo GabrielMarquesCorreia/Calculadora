244c63ce6c28b66282a4ecd576dfb0bb9c8bd7a3
Gabriel Marques Correia <gabrielmarquescorreia4@gmail.com>
2024-10-23 11:01:58 -0300

    Atualizando

--- Calculadora.py
+++ Calculadora.py
@@ -1,17 +1,28 @@
-print("Calculadora")
-n1 = float(input("Digite um numero : "))
-op = input("Digite (+) para soma , (-) para subtracao , (%) para divisao e (x) para multiplicacao : ")
-n2 = float(input("Digite outro numero : "))
-if op == '+':
-    r = n1 + n2
-    print("A soma entre {} e {} e igual a : {}.".format(n1 , n2 , r))
-elif op == '-':
-    r = n1 - n2
-    print("A subtracao entre {} e {} e igual a : {}".format(n1 , n2 , r))
-elif op == '%':
-    r = n1 / n2
-    print("A divisao entre {} e {} e igual a : {}".format(n1 , n2 , r))
-elif op == 'x':
-    r = n1 * n2
-    print("A multiplicacao entre {} e {} e igual a : {}".format(n1 , n2 , r))
-    
+def calculadora():
+    print("\nCalculadora")
+    n1 = float(input("\nDigite um número: "))
+    op = input("Digite (+) para soma, (-) para subtração, (/) para divisão e (x) para multiplicação: ")
+    n2 = float(input("Digite outro número: "))
+
+    if op == '+':
+        r = n1 + n2
+        print("\nA soma entre {} e {} é igual a: {}.".format(n1, n2, r))
+    elif op == '-':
+        r = n1 - n2
+        print("\nA subtração entre {} e {} é igual a: {}.".format(n1, n2, r))
+    elif op == '/':
+        r = n1 / n2
+        print("\nA divisão entre {} e {} é igual a: {}.".format(n1, n2, r))
+    elif op == 'x':
+        r = n1 * n2
+        print("\nA multiplicação entre {} e {} é igual a: {}.".format(n1, n2, r))
+    else:
+        print("\nOperação inválida.")
+
+    op2 = input("\nDeseja fazer outra conta? sim(s) não(n): ")
+    if op2 == 's':
+        calculadora()
+    else:
+        exit()
+
+calculadora()
